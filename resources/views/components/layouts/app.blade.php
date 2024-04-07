adf4ecbe5ce2a87d6669e822405be99ea4bab35c
Delendins <nastyraven123@gmail.com>
2024-04-08 03:19:50 +0700

    fixing "This page has expired"

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf_token" value="{{ csrf_token() }}"/>
 
     <title>{{ $title ?? 'Kasir App' }} | Kasir App</title>
 
