4cc99e7f8b12b9370068f6ae427120cdd7840457
mohfer <nastyraven123@gmail.com>
2025-08-11 00:37:46 +0700

    Add Umami Analytics script to the layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,9 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf_token" value="{{ csrf_token() }}"/>
+    <meta name="csrf_token" value="{{ csrf_token() }}" />
 
     <title>{{ $title ?? 'Kasir App' }} | Kasir App</title>
+
+    <!-- Umami Analytics -->
+    <script defer src="https://umami.mohfer.my.id/script.js" data-website-id="8ab0acd8-43bc-4595-ae61-aa25a8baaa92">
+    </script>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
